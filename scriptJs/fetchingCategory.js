61f9ed84eea9105d45335476770fbd65f66e7bcd
asifmahmud1047 <asif.mahmud1047@gmail.com>
2024-10-11 16:05:20 +0600

    almost done

--- scriptJs/fetchingCategory.js
+++ scriptJs/fetchingCategory.js
@@ -13,18 +13,3 @@ fetch("https://openapi.programming-hero.com/api/peddy/categories")
       categoriesContainer.appendChild(categoryButton);
     });
   });
-// .catch((error) => console.error("Error fetching categories:", error));
-
-// fetch("https://openapi.programming-hero.com/api/peddy/categories")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const categories = data.categories;
-//     categories.forEach((category) => {
-//       document.getElementById("categories").innerHTML += `
-//       <button class="category-button rounded-3xl flex items-center justify-center">
-//           <img src="${category.category_icon}" alt="${category.category} icon" class="category-icon mr-2">
-//           ${category.category}
-//         </button>
-//       `;
-//     });
-//   });
